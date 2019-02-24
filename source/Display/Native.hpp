63a9769e854344d3af6bdd42fdb44c243bff9b27
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-02-25 11:18:29 +1300

    Add Native namespace.

---
+++ source/Display/Native.hpp
@@ -0,0 +1,17 @@
+//
+//  Native.hpp
+//  This file is part of the "Vizor Platform XCB" project and released under the .
+//
+//  Created by Samuel Williams on 25/2/2019.
+//  Copyright, 2019, by Samuel Williams. All rights reserved.
+//
+
+#pragma once
+
+#include "XCB/Application.hpp"
+#include "XCB/Window.hpp"
+
+namespace Display
+{
+	namespace Native = XCB;
+}
